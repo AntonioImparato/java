b70f4a868003eb0eb924ef9747ee6762e6cc098f
Antonio Imparato <antonioimparato16@gmail.com>
2024-06-26 13:00:02 +0200

    classe aquila

---
+++ abstract-animals/src/org/lessons/java/animals/Aquila.java
@@ -0,0 +1,23 @@
+package org.lessons.java.animals;
+
+public class Aquila extends Animale {
+
+	@Override
+	public void dormi() {
+		System.out.println(" Zzzz ");
+		
+	}
+
+	@Override
+	public void verso() {
+		System.out.println(" Kreek ");
+		
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println(" l'aquila mangia mammiferi di piccolo e media taglia  ");
+		
+	}
+
+}
