7322203167397b013e15d9cd57e2fcf71d035249
Antonio Imparato <antonioimparato16@gmail.com>
2024-06-26 12:49:42 +0200

    classe passerotto

---
+++ abstract-animals/src/org/lessons/java/animals/Passerotto.java
@@ -0,0 +1,24 @@
+package org.lessons.java.animals;
+
+public class Passerotto extends Animale{
+	
+	@Override
+	public void dormi() {
+		System.out.println(" Zzzz ");
+		
+	}
+
+	@Override
+	public void verso() {
+		System.out.println(" cip cip");
+		
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println(" il passerotto mangia semi o insetti");
+		
+	}
+
+}
+
