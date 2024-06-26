7dd2882189ca247d1cca7a9be581ac935b8aab36
Antonio Imparato <antonioimparato16@gmail.com>
2024-06-26 13:04:28 +0200

    classe delfino

---
+++ abstract-animals/src/org/lessons/java/animals/Delfino.java
@@ -0,0 +1,23 @@
+package org.lessons.java.animals;
+
+public class Delfino extends Animale {
+
+	@Override
+	public void dormi() {
+		System.out.println(" Zzzz ");
+		
+	}
+
+	@Override
+	public void verso() {
+		System.out.println(" Whistle, Whistle ");
+		
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println(" Il delfino mangia Pesci, crostaacei e molluschi");
+		
+	}
+
+}
