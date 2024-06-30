658ce8c95f5c088423699d6a60cb975c45cf65b3
Antonio Imparato <antonioimparato16@gmail.com>
2024-06-30 22:56:34 +0200

    metodi set e get Concerto

--- java-gestore-eventi/src/org/lessons/java/gestore/eventi/Concerto.java
+++ java-gestore-eventi/src/org/lessons/java/gestore/eventi/Concerto.java
@@ -1,5 +1,32 @@
 package org.lessons.java.gestore.eventi;
 
-public class Concerto {
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Concerto extends Evento{
+	private LocalTime ora ;
+	private float prezzo;
+	public Concerto(String titolo, LocalDate data, int numeroPostiTotali, float prezzo,LocalTime ora ) {
+		super(titolo, data, numeroPostiTotali);
+		this.prezzo = prezzo;
+		this.ora  = ora;
+		
+
+	}
+	public float getPrezzo() {
+		return this.prezzo;
+	}
+	
+	public void setPrezzo(float prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	public LocalTime getora() {
+		return this.ora;
+	}
+	
+	public void setOra(LocalTime ora) {
+		this.ora = ora;
+	}
 
 }
