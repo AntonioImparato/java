cb9bc6617e9b42f3850927fb54b56aef2877239b
Antonio Imparato <antonioimparato16@gmail.com>
2024-07-05 09:54:44 +0200

    aggiunti metodi alla classe ProgrammaEventi

--- java-gestore-eventi/src/org/lessons/java/gestore/eventi/ProgrammaEventi.java
+++ java-gestore-eventi/src/org/lessons/java/gestore/eventi/ProgrammaEventi.java
@@ -1,5 +1,18 @@
 package org.lessons.java.gestore.eventi;
 
-public class ProgrammaEventi {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ProgrammaEventi {
+    private String titolo;
+    private List<Evento> eventi;
+
+    public ProgrammaEventi(String titolo) {
+        this.titolo = titolo;
+        this.eventi = new ArrayList<>();
+    }
+    
+    public void AggiungiTitolo(String evento) {
+    	eventi.add(avento);
+    }
 }
