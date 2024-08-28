5a91df7276baede465f65a43298ba7b2acaebd83
Antonio Imparato <antonioimparato16@gmail.com>
2024-08-28 16:48:21 +0200

    step 1 best-of-the-year

---
+++ best-of-the-year/src/test/java/best/of/the/year/java/BestOfTheYearApplicationTests.java
@@ -0,0 +1,13 @@
+package best.of.the.year.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfTheYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
