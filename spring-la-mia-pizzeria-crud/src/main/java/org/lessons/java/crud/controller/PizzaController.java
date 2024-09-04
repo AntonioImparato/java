65a1aaa602508beca6f17615de3af21f399c7641
Antonio Imparato <antonioimparato16@gmail.com>
2024-09-04 14:05:45 +0200

    form in idex e show

--- spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/crud/controller/PizzaController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/crud/controller/PizzaController.java
@@ -5,35 +5,44 @@ import java.util.List;
 import org.lessons.java.crud.model.Pizza;
 import org.lessons.java.crud.repo.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/pizze")
 public class PizzaController {
 
-	@Autowired
-	private PizzaRepository repo;
-	
-	@GetMapping()
-	public String index (Model model) {
-		
-		List<Pizza> pizze = repo.findAll();
-		model.addAttribute("pizze", pizze);
-		return "/pizze/index";
-	} 
+    @Autowired
+    private PizzaRepository repo;
+    
+    @GetMapping()
+    public String index(Model model, @RequestParam(name = "nome", required = false) String nome) {
+        
+        List<Pizza> pizze;
+        if (nome != null && !nome.isEmpty()) {
+            pizze = repo.findByNomeContains(nome);
+        } else {
+            pizze = repo.findAll(Sort.by("nome"));
+        }
+        
+        model.addAttribute("pizze", pizze);
+        model.addAttribute("nome", nome); // Aggiungi il nome al modello
+        return "/pizze/index";
+    } 
 
-	@GetMapping("/findByNome/{nome}")
-	public String findByNome(@PathVariable("nome") String nome , Model model) {
-		model.addAttribute("pizze", repo.findByNomeContains(nome));
-		return "/pizze/index";
-	}
-	@GetMapping("/show/{id}")
-	public String show(@PathVariable("id") Integer id , Model model) {
-		model.addAttribute("pizza", repo.findById(id).get());
-		return "/pizze/show";
-	}
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("pizza", repo.findById(id).get());
+        return "/pizze/show";
+    }
+    
+    public String listPizze(@RequestParam(required = false) String nome, Model model) {
+        model.addAttribute("nome", nome);
+        return "pizze";
+    }
 }
