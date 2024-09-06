16204642595825c637aee3c2a8dd587bcb0fd5a1
Antonio Imparato <antonioimparato16@gmail.com>
2024-09-06 14:23:11 +0200

    add : delete

--- spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/crud/controller/PizzaController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/crud/controller/PizzaController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.validation.Valid;
 
@@ -22,49 +23,88 @@ import jakarta.validation.Valid;
 @RequestMapping("/pizze")
 public class PizzaController {
 
-    @Autowired
-    private PizzaRepository repo;
-    
-    @GetMapping()
-    public String index(Model model, @RequestParam(name = "nome", required = false) String nome) {
-        
-        List<Pizza> pizze;
-        if (nome != null && !nome.isEmpty()) {
-            pizze = repo.findByNomeContains(nome);
-        } else {
-            pizze = repo.findAll(Sort.by("nome"));
-        }
-        
-        model.addAttribute("pizze", pizze);
-        return "/pizze/index";
-    } 
+	// collegamento alla repositery
+	@Autowired
+	private PizzaRepository repo;
 
-    @GetMapping("/show/{id}")
-    public String show(@PathVariable("id") Integer id, Model model) {
-        model.addAttribute("pizza", repo.findById(id).get());
-        return "/pizze/show";
-    }
-    
-    @GetMapping("/create")
-    public String add( Model model) {
-    	 model.addAttribute("pizza", new Pizza());
-        return "pizze/create";
-    }
-    
-    @PostMapping("/create")
-    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, 
-    					BindingResult bindingResult,
-    					Model model )
-    {
-    	if(bindingResult.hasErrors()) {
-    		return"pizze/create";
-    	}
-    	 repo.save(formPizza);
-    	return"redirect:/pizze";
-    }
-    
-    public String listPizze(@RequestParam(required = false) String nome, Model model) {
-        model.addAttribute("nome", nome);
-        return "pizze";
-    }
+	// index
+	@GetMapping()
+	public String index(Model model, @RequestParam(name = "nome", required = false) String nome) {
+
+		List<Pizza> pizze;
+		if (nome != null && !nome.isEmpty()) {
+			pizze = repo.findByNomeContains(nome);
+		} else {
+			pizze = repo.findAll(Sort.by("nome"));
+		}
+
+		model.addAttribute("pizze", pizze);
+		return "/pizze/index";
+	}
+
+	// show
+	@GetMapping("/show/{id}")
+	public String show(@PathVariable("id") Integer id, Model model) {
+		model.addAttribute("pizza", repo.findById(id).get());
+		return "/pizze/show";
+	}
+
+	@GetMapping("/create")
+	public String add(Model model) {
+		model.addAttribute("pizza", new Pizza());
+		return "/pizze/create";
+	}
+
+	// create
+	@PostMapping("/create")
+	public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, 
+			BindingResult bindingResult, Model model,
+			RedirectAttributes attributes) {
+		if (bindingResult.hasErrors()) {
+			return "/pizze/create";
+		}
+		repo.save(formPizza);
+		attributes.addFlashAttribute("successMessage", formPizza.getNome() + " aggiunta con successo " );
+		return "redirect:/pizze";
+	}
+
+	public String listPizze(@RequestParam(required = false) String nome, Model model) {
+		model.addAttribute("nome", nome);
+		return "/pizze";
+	}
+
+	// edit
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Integer id, Model model) {
+//    	Pizza pizza =repo.findById(id).get();
+//    	model.addAttribute("pizza", pizza);
+
+		model.addAttribute("pizza", repo.findById(id).get());
+		return "/pizze/edit";
+	}
+
+	// Update
+	@PostMapping("/edit/{id}")
+	public String update(@Valid @ModelAttribute("pizza") Pizza updateFormPizza, 
+			BindingResult bindingResult,
+			Model model,
+			RedirectAttributes attributes) {
+		if (bindingResult.hasErrors()) {
+			return "/pizze/edit";
+		}
+		repo.save(updateFormPizza);
+		attributes.addFlashAttribute("successMessage", updateFormPizza.getNome() + " modificata" );
+		return "redirect:/pizze";
+	}
+	
+	//delite
+	@PostMapping("delete/{id}")
+		public String delite(@PathVariable("id") Integer id,
+				RedirectAttributes attributes){
+		
+			repo.deleteById(id);
+			attributes.addFlashAttribute("deleteMessage", "Pizza eliminata" );
+			return"redirect:/pizze";
+		}
+	
 }
